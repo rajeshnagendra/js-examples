2490b0fded64cb74c2c3c231b564e0460a9241e0
Rajesh Nagendra <rajeshnraghav@gmail.com>
2018-01-06 15:54:58 +0530

    js examples

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # js-examples
 javascript examples!
+
+Higher order functions are:     
+    -functions that manipulate other functions.
+    -take other functions as arguments and/or produce a function as its RETURN value
--- high-order-functions.js
+++ high-order-functions.js
@@ -1,3 +1,5 @@
+//reference link https://gitbookio.gitbooks.io/javascript/functions/higher_order.html
+
 var add = function(x){
     return x + 2;
 }
@@ -5,4 +7,38 @@ var add = function(x){
 var double = function(x){
     return x * 2;
 }
+//func - function to call, //list - array of values to call func
+var map = function(func, list){
+    var output = [];
+    for(idx in list){
+        output.push(func(list[idx]));
+    }
+    return output;
+}
 
+//console output
+map(add, [2,4,6]); //returns an array [4,6,8]
+map(double, [5,20,50]); //returns array [10,40,100]
+//-----------------------------------------------------------
+process_add = function(list){
+	return map(add, list);
+}
+
+process_double = function(list){
+	return map(double, list);
+}
+process_add([2,4,6]);
+process_double([5,20,50]);
+//-----------------------------------------------------------
+var buildProcessor = function(func){
+    var process_func = function(list){
+        return map(func,list);
+    }
+    return process_func;
+}
+
+process_add = buildProcessor(add);
+process_double = buildProcessor(double);
+
+process_add([2,4,6]);
+process_double([5,20,50]);
