6fb897b33bcfaa2bbe3d74998e32c79efcb556f0
Rajesh Nagendra <rajeshnraghav@gmail.com>
2018-01-06 23:31:01 +0530

    examples

--- objects-example.js
+++ objects-example.js
@@ -20,6 +20,45 @@ var language = {
     getAuthorFullName: function(){
         return this.author.fName + " " + this.author.lName;
     }    
-    
 }
+console.log(language);
+
+console.log(language.name); /*OR*/ console.log(language['name']);
+//adding a new property to language
+language.newProp = 'some value';
+language["newProp"]="updated value" //newProp - remember case sensitive!!
+
+console.log(language);
+
+//immutable
+var myVal = "javascript";
+myVal = "ES6";
+console.log(myVal)
+
+// mutable
+var myObject = {city:'bangalore'};
+myObject.city = 'mysore'; //points to same object after updating the property
+console.log(myObject);
+
+//references!
+var myBook = {name:'Tinkle'}
+var yourBook = myBook;
+
+myBook.name += " Subscription"
+console.log(yourBook);
+
+
+var a = {}, b= {}, c = {}; // all three objects are different here
+
+a = b = c = {}; //reference objects. changes all three on every change.
+
+a.something = "adding property that affects all three objects";
+console.log(b,c);
+
+
+
+
+
+
+
 
