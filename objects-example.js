61e05f90f112a1ca6f2c46e2780a78d6d4f979fa
Rajesh Nagendra <rajeshnraghav@gmail.com>
2018-01-06 23:28:44 +0530

    updated!

--- objects-example.js
+++ objects-example.js
@@ -6,3 +6,20 @@ var obj = {};
 
 //2. object oriented way!
 var obj = new Object();
+
+//3. Properties
+
+var language = {
+    name:'Js',
+    createdIn:1995,
+    browserSupport: true,
+    author:{
+        fName:"Brendan",
+        lName: "Eich"
+    },
+    getAuthorFullName: function(){
+        return this.author.fName + " " + this.author.lName;
+    }    
+    
+}
+
